2bd10759b2d15d013061012377f81a36c8788f17
Ilya Myshkin <im@versta24.ru>
2024-02-17 13:20:31 +0300

    Add project files.

---
+++ Entites/RawMaterials/RawMaterial.cs
@@ -0,0 +1,15 @@
+﻿namespace Entites.RawMaterials;
+
+public class RawMaterial
+{
+
+}
+
+
+/// <summary>
+///  Тип сырья, из чего состоит 
+/// </summary>
+public enum RawMaterialType
+{
+    Diamond
+}
