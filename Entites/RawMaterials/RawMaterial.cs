4ad704bd34339d898b5df6ca77603cc654a44dc4
Ilya Myshkin <im@versta24.ru>
2024-02-17 13:23:29 +0300

    feat: added raw material enity

--- Entites/RawMaterials/RawMaterial.cs
+++ Entites/RawMaterials/RawMaterial.cs
@@ -1,8 +1,12 @@
 ﻿namespace Entites.RawMaterials;
 
+/// <summary>
+/// Сырье 
+/// </summary>
 public class RawMaterial
 {
-
+    public RawMaterialType Type { get; set; }
+    public int Mass { get; set; }
 }
 
 
